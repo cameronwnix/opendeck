ad6a56592a29431ebb7c143fbee72691742a9f44
cameronwnix <cameronwnix@gmail.com>
2023-10-11 16:09:05 -0400

    Starting to figure out shuffling function

--- deck.h
+++ deck.h
@@ -4,24 +4,26 @@
 using namespace std;
 
 class Deck{
-    private:
-        //Variables, Objects
-        char Suits[4]{'C', 'D', 'H', 'S'}; // Array of card suits represented by the first char of their names (Clubs, Diamonds, Hearts, Spades). Used by genCard().
-        char Ranks[13]{'2', '3', '4', '5', '6', '7', '8', '9', 'X', 'J', 'Q', 'K', 'A'}; // Array of card ranks represented by chars (10 is represented as X). Used by genCard().
-        struct Card{char suit; char rank; string cardId;} _card; // Struct representing a card. 
-        
-        //Functions
-        void buildDeck();
-        Card genCard(char dSuit, char dRank); // Generates each Card. Used by buildDeck().
-
     public:
         //Constructor
         Deck();
 
         //Variables, Objects
+        struct Card{char suit; char rank; string cardId;} _card; // Struct representing a card.
         Card newDeck[52]; // Array of Cards representing the Deck. Used by buildDeck().
 
         //Functions
+    private:
+        //Variables, Objects
+        char Suits[4]{'C', 'D', 'H', 'S'}; // Array of card suits represented by the first char of their names (Clubs, Diamonds, Hearts, Spades). Used by genCard().
+        char Ranks[13]{'2', '3', '4', '5', '6', '7', '8', '9', 'X', 'J', 'Q', 'K', 'A'}; // Array of card ranks represented by chars (10 is represented as X). Used by genCard().
+         
+        
+        //Functions
+        void buildDeck();
+        Card genCard(char dSuit, char dRank); // Generates each Card. Used by buildDeck().
+
+
 
 };
 
--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <stdlib.h>
+#include <time.h>
 #include "deck.h"
 
 using namespace std;
@@ -10,13 +12,28 @@ int main()
     Deck Deck1; // Instantiate our Deck.
 
     // Starting to build out the shuffling algorithm below:
+    srand(time(NULL)); // Seed rand() with current time()
+    int breakRand{rand() % 11+45}; // Sets int breakRand to a random number between 45 and 55 (representing the percentage of the deck randomly grabbed when breaking the deck prior to a riffle)
+    int partSize{(breakRand * 52)/100}; // Determine how many cards the percentage of the deck decided above actually comes out to and store in partSize
+    Deck::Card rifflePart[partSize]; // Instantiate an array holding the number of cards to be separated out (represented by partSize)
     
+    for (int i = 0; i < partSize; i++)
+    {
+        rifflePart[i]{Deck1.newDeck[i]};
+        
+    }
+
+    cout << partSize;
 
     // Iterates through the filled newDeck[] and prints the CardId of each Card. For testing and verification.
+    /*
     for (int i = 0; i < 52; i++) {
         cout << Deck1.newDeck[i].cardId+"\n";
     }
-    
+    */
+
+
+
     // This is my emotional support string.
    string myString{"This is a string"};
    cout << myString;
